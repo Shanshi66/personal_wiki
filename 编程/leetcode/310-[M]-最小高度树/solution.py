d9d1b8de0d8dd0ace85669e03fb9c388fd208fc6
Rosen <heyrosen@163.com>
2022-08-20 23:56:53 +0800

    2022-08-20
    Change-Id: I71968764a4c3d9cde8cf74ff6d51239b2ebf5b2a

--- 编程/leetcode/310-[M]-最小高度树/solution.py
+++ 编程/leetcode/310-[M]-最小高度树/solution.py
@@ -1,103 +1,63 @@
 from collections import deque
+from enum import Flag
 import functools
 from typing import List
 
-
-# class Solution:
-#     # def solve(self, dp, u, v):
-#     #     if dp[u][v] != -1:
-#     #         return dp[u][v]
-#     #     for w in dp[v]:
-#     #         if w == u:
-#     #             continue
-#     #         dp[u][v] = max(dp[u][v], self.solve(dp, v, w)+1)
-#     #     if dp[u][v] == -1:
-#     #         dp[u][v] = 0
-#     #     return dp[u][v]
-
-#     def solve(self, dp, u, v):
-#         if dp[u][v] != -1:
-#             return dp[u][v]
-#         stack = [(u, v)]
-#         while stack:
-#             _u, _v = stack[-1]
-#             if not dp[_v]:
-#                 dp[_u][_v] = 1
-#                 stack.pop()
-#                 continue
-#             for s in dp[_v]:
-#                 if _u == s:
-#                     continue
-#                 if dp[_v][s] == -1:
-#                     stack.append((_v, s))
-#                 else:
-#                     dp[_u][_v] = max(dp[_u][_v], dp[_v][s]+1)
-            
-            
-                
-            
-
-#     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
-#         dp = {}
-#         for edge in edges:
-#             dp.setdefault(edge[0], {})
-#             dp.setdefault(edge[1], {})
-#             dp[edge[0]][edge[1]] = -1
-#             dp[edge[1]][edge[0]] = -1
-#         result = {}
-#         min_height = n
-#         for u in dp:
-#             height_ = -1
-#             for v in dp[u]:
-#                 dp[u][v] = self.solve(dp, u, v)
-#                 height_ = max(height_, dp[u][v])
-#             min_height = min(height_, min_height)
-#             result.setdefault(height_, []).append(u)
-#         if min_height == n:
-#             return [0]
-#         return result[min_height]
-
+# 最小高度树的根一定在最长路径上，且是中点(如果路径长度为奇数是中点，如果是偶数，中间的pair都可以作为根)。可以反证法证明，如果不是，有两种情况，1）根在最长路径上，但不在中点上，那么根节点与最长路径远端的距离一定大于最长路径的一半；2）根不在最长路径上，根到叶子节点必定要与最长路径有交点，那么最小高度一定比最长路径的一半要长
+# 拓扑排序：依次删除度为1的点，剩下的位最长路径下剩余的点
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         if n == 1:
             return [0]
+        if n == 2:
+            return [0, 1]
+        graph = {}
+        degree = [0]*n
+        for edge in edges:
+            graph.setdefault(edge[0], []).append(edge[1])
+            graph.setdefault(edge[1], []).append(edge[0])
+            degree[edge[0]] += 1
+            degree[edge[1]] += 1
 
-        g = [[] for _ in range(n)]
-        for x, y in edges:
-            g[x].append(y)
-            g[y].append(x)
-        parents = [0] * n
+        node_left = n
+        delete = [False]*n
 
-        def bfs(start: int):
-            vis = [False] * n
-            vis[start] = True
-            q = deque([start])
-            while q:
-                x = q.popleft()
-                for y in g[x]:
-                    if not vis[y]:
-                        vis[y] = True
-                        parents[y] = x
-                        q.append(y)
-            return x
-        x = bfs(0)  # 找到与节点 0 最远的节点 x
-        y = bfs(x)  # 找到与节点 x 最远的节点 y
+        delete_node = []
+        for i in range(n):
+            if degree[i] == 1:
+                delete_node.append(i)
+                delete[i] = True
+                node_left -= 1
+        
+        while node_left > 2:
+            tmp = []
+            for node in delete_node:
+                for son in graph[node]:
+                    if delete[son]:
+                        continue
+                    degree[son] -= 1
+                    if degree[son] == 1:
+                        tmp.append(son)
+                        delete[son] = True
+                        node_left -= 1
+            delete_node = tmp
+        result = set()
+        for node in delete_node:
+            for son in graph[node]:
+                if not delete[son]:
+                    result.add(son)
+        return list(result)
 
-        path = []
-        parents[x] = -1
-        while y != -1:
-            path.append(y)
-            y = parents[y]
-        m = len(path)
-        print(path)
-        return [path[m // 2]] if m % 2 else [path[m // 2 - 1], path[m // 2]]
-
+# 也可以先找出最长路径，再取路径中点
+# 求最长路径的方法：以任意顶点p出发，找距离最远的顶点x，以x出发，找距离x最远的顶点y，xy为最长路径
 
 if __name__ == '__main__':
     test = Solution()
-    # print(test.findMinHeightTrees(4, [[1,0],[1,2],[1,3]]))
+    print(test.findMinHeightTrees(4, [[1,0],[1,2],[1,3]]))
     print(test.findMinHeightTrees(6, [[3,0],[3,1],[3,2],[3,4],[5,4]]))
-    # print(test.findMinHeightTrees(1, []))
+    print(test.findMinHeightTrees(1, []))
+    print(test.findMinHeightTrees(2, [[0,1]]))
+    print(test.findMinHeightTrees(3, [[0,1], [0,2]]))
 
         
 
