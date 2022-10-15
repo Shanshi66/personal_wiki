6068b65e4b60c3b83b0431239ad9bb91d4ea794e
luosen <heyrosen@163.com>
2022-10-15 16:56:33 +0800

    2022-10-15

--- 编程/leetcode/769-[M]-最多能完成排序的块/solution.py
+++ 编程/leetcode/769-[M]-最多能完成排序的块/solution.py
@@ -1,5 +1,6 @@
 from typing import List
 
+# 思路：因为只有0 -> n-1这n个数，可以从最大数开始遍历起截取块，最大数到数组末尾一定属于一个块。如果当前数的index在已取得块之外，小于后面的最小值，则可以形成新的块。
 
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
@@ -24,6 +25,17 @@ class Solution:
             i = min(i, idx)
         return cnt
 
+# 官方思路：
+# 因为数的范围是0 -> n-1, 排序好之后数i一定在i位置。因此可以遍历数组，如果max(a[0]..a[i]) == i, 则可以形成一个块。
+# 问题：会不会存在最小值在很后面的情况？如果一个比较小的值在很后面，那么必定会交换到一个更大的值，当前最大值一定大于i。
+
+class Solution:
+    def maxChunksToSorted(self, arr: List[int]) -> int:
+        ans = mx = 0
+        for i, x in enumerate(arr):
+            mx = max(mx, x)
+            ans += mx == i
+        return ans
 
 if __name__ == '__main__':
     test = Solution()
