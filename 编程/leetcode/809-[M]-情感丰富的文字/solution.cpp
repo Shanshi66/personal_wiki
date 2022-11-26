fdc150453f40423dd6d5e46bc21d9f59465c288e
luosen <heyrosen@163.com>
2022-11-26 22:51:55 +0800

    2022-11-25

---
+++ 编程/leetcode/809-[M]-情感丰富的文字/solution.cpp
@@ -0,0 +1,12 @@
+#include<string>
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int expressiveWords(string s, vector<string>& words) {
+        
+    }
+};
